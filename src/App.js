26b0ef01d5804746d3d7517a34160b22cfc83f09
Jhed Mendoza <jhed.mendoza@hotmail.com>
2021-08-06 10:36:47 +0800

    Added Click counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0)
+  let marginRight = {marginRight: '10px'}
+
+  const handleIncrementEvent = () => {
+   setCount(count + 1)
+  }
+
+  const handleResetEvent = () => {
+    setCount(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div class="container">
+      <div class="card mt-5 w-25">
+        <h5 class="card-header">Click Counter</h5>
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text">Count: {count}</p>
+          <button class="btn btn-primary" onClick={handleIncrementEvent} style={marginRight}>Click Me!</button>
+          <button class="btn btn-success text-white" onClick={handleResetEvent}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
