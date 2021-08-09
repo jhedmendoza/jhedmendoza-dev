d9badecb55142d051f6bd16123085e93be8651dd
Jhed Mendoza <jhed.mendoza@hotmail.com>
2021-08-09 18:32:10 +0800

    Integrated dictionary API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
 
-  return (
-    <div class="featurette">
-    <div class="featurette-inner text-center">
-       <div role="form" class="search">
-          <div class="input-group input-group-lg">
-             <input type="search" placeholder="Type words in here" class="form-control" />
-             <span class="input-group-btn">
-             <button class="btn btn-success btn-search" type="button">Search</button>
-             </span>
-          </div>
-       </div>
-    </div>
- </div>
+  axios.defaults.baseURL = 'https://api.dictionaryapi.dev/api/v2/entries';
 
+  const [data, setData] = useState({
+    search: ""
+  }) 
+
+  const [results, setResult] = useState('');
+
+  const handleChange = (event) => {
+    const newData = {...data}
+    newData[event.target.id] = event.target.value
+    setData(newData)
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    let textToSearch = data.search
+    axios.get('en_US/'+textToSearch).then( res => 
+      setResult(res.data)
+    )
+  };
+
+  return (
+   <div className="wrapper">
+    <div className="featurette">
+      <div className="featurette-inner text-center">
+        <form className="search" onSubmit={(e)=> handleSubmit(e)}>
+            <div className="input-group input-group-lg">
+              <input type="text" id="search" placeholder="Type words in here" onChange={(e) => handleChange(e)} value={data.search} className="form-control" />
+              <input className="btn btn-success btn-search" type="submit" value="Search" />
+            </div>
+        </form>
+      </div>
+  </div>
+    {
+      results.length ? results.map((value, index) => {
+        console.log(value)
+        return <div className="container container-meanings mb-3" key={index}>
+                <div className="row">
+                  <h2>
+                  {value.word} 
+
+                  { value.meanings.map((meanings, keymeanings) => 
+                      <span className="part-of-speech" key={keymeanings}> {meanings.partOfSpeech}, </span> 
+                  )}
+                  
+                  </h2>
+                  <h4>{value.phonetic}</h4>
+                  <p>{value.origin}</p>
+                </div>
+              </div>
+      }) : ""
+    }
+  </div>  
   );
 }
 
