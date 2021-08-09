45f25e362bbdeccfe33d06c47e4249b921fc57fb
Jhed Mendoza <jhed.mendoza@hotmail.com>
2021-08-09 15:51:10 +0800

    Fixed compiler error

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
     <div class="featurette">
     <div class="featurette-inner text-center">
-       <form role="form" class="search">
+       <div role="form" class="search">
           <div class="input-group input-group-lg">
              <input type="search" placeholder="Type words in here" class="form-control" />
              <span class="input-group-btn">
              <button class="btn btn-success btn-search" type="button">Search</button>
              </span>
           </div>
-       </form>
+       </div>
     </div>
  </div>
 
