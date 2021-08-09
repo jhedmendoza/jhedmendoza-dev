5fb9784a67c4bd53587a46c2febf379046b1fccf
Jhed Mendoza <jhed.mendoza@hotmail.com>
2021-08-09 15:44:32 +0800

    Added search box

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  let [count, setCount] = useState(0)
-  let marginRight = {marginRight: '10px'}
-
-  const handleIncrementEvent = () => {
-   setCount(count + 1)
-  }
-
-  const handleResetEvent = () => {
-    setCount(0)
-  }
 
   return (
-    <div class="container">
-      <div class="card mt-5 w-25">
-        <h5 class="card-header">Click Counter</h5>
-        <div class="card-body">
-          <p class="card-text">Count: {count}</p>
-          <button class="btn btn-primary" onClick={handleIncrementEvent} style={marginRight}>Click Me!</button>
-          <button class="btn btn-success text-white" onClick={handleResetEvent}>Reset</button>
-        </div>
-      </div>
+    <div class="featurette">
+    <div class="featurette-inner text-center">
+       <form role="form" class="search">
+          <div class="input-group input-group-lg">
+             <input type="search" placeholder="Type words in here" class="form-control" />
+             <span class="input-group-btn">
+             <button class="btn btn-success btn-search" type="button">Search</button>
+             </span>
+          </div>
+       </form>
     </div>
+ </div>
+
   );
 }
 
